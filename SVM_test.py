89864f4a0b1985b2b480f0e064b527573c0f9420
Leyu Yao <yao.ly@qq.com>
2017-12-04 20:24:28 +0800

    prototype

--- SVM_test.py
+++ SVM_test.py
@@ -6,14 +6,30 @@ Created on Sun Oct 22 10:23:45 2017
 """
 import Algorithms
 import numpy
+import Dataset
+
+
 
 X = numpy.array([[1,3,3],[1,2,3]])
 
 Y = numpy.array([[1],[2]])
 
+NorX = Dataset.Normalization()
+
+
+NorX.fit(X)
+
+
+NorY = Dataset.Normalization()
+NorY.fit(Y)
+
+X_N = NorX.forwardTransform()
+
+Y_N = NorY.forwardTransform()
+
 svm = Algorithms.SVM()
 
-svm.Data.load(X,Y)
+svm.Data.load(X_N,Y_N)
 
 #svm.Data.set_up_for_SVM()
     
